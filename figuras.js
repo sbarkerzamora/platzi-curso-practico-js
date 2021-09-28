2f9d9d4837144f43a7908b2de639009990914bae
Stephan Barker <sbarkerzamora@gmail.com>
2021-09-27 21:54:51 -0600

    Update figuras.js

--- figuras.js
+++ figuras.js
@@ -39,3 +39,29 @@ const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
 console.log('El area del triangulo es ' + areaTriangulo + ' cm^2');
 
 console.groupEnd();
+
+//Codigo del circulo
+
+console.group('Triangulos');
+
+// Radio
+const radioCirculo = 4;
+console.log('El radio del circulo es ' + radioCirculo + ' cm');
+
+// Diametro
+const diametroCirculo = radioCirculo * 2;
+console.log('El diametro del circulo es ' + diametroCirculo + ' cm');
+
+// PI
+const PI = Math.PI;
+console.log('PI es ' + PI + ' cm');
+
+// Circunferencia
+const perimetroCirculo = diametroCirculo * PI;
+console.log('El perimetro del circulo es ' + perimetroCirculo + ' cm');
+
+// Area
+const areaCirculo = (diametroCirculo * radioCirculo) * PI;
+console.log('El area del circulo es ' + areaCirculo + ' cm^2');
+
+console.groupEnd();
