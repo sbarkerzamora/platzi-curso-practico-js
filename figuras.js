1d2b9f0efb4fb17b464692f4afcaefd49959f39f
Stephan Barker <sbarkerzamora@gmail.com>
2021-09-28 10:15:52 -0600

    Update figuras.js

--- figuras.js
+++ figuras.js
@@ -53,9 +53,15 @@ console.groupEnd();
 // Aqui interactuamos con el html
 
 function calcularPerimetroCuadrado() {
-
-}
+  const input = document.getElementById('InputCuadrado');
+  const value = input.value;
+  const perimetro = perimetroCuadrado(value);
+  alert(perimetro);
+};
 
 function calcularAreaCuadrado() {
-
-}
+  const input = document.getElementById('InputCuadrado');
+  const value = input.value;
+  const area = areaCuadrado(value);
+  alert(area);
+};
