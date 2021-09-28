2a28750819257ae6038ad6c1874a92184b6eecb8
Stephan Barker <sbarkerzamora@gmail.com>
2021-09-27 21:34:07 -0600

    Update figuras.js

--- figuras.js
+++ figuras.js
@@ -1,8 +1,26 @@
+//Codigo del cuadrado
+
 const ladoCuadrado = 5;
-console.log('Los lados del cuadrado miden: ' + ladoCuadrado) + ' cm';
+console.log('Los lados del cuadrado miden: ' + ladoCuadrado + ' cm');
 
 const perimetroCuadrado = ladoCuadrado * 4;
 console.log('El perimetro del cuadrado es ' + perimetroCuadrado + ' cm');
 
 const areaCuadrado = ladoCuadrado * ladoCuadrado;
-console.log('El el area del cuadrado es ' + areaCuadrado + ' cm2');
+console.log('El el area del cuadrado es ' + areaCuadrado + ' cm^2');
+
+
+//Codigo del triangulo
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+console.log('Los lados del triangulo miden: '
+  + ladoTriangulo1
+  + ' cm,'
+  + ladoTriangulo2
+  + ' cm,'
+  + baseTriangulo
+  + ' cm'
+);
