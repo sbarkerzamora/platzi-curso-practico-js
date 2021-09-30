ba1168025f755beac5199ebcc58fcf637955c352
Stephan Barker <sbarkerzamora@gmail.com>
2021-09-30 10:54:38 -0600

    Cambios

--- descuento.js
+++ descuento.js
@@ -3,3 +3,16 @@ function calcularDescuento(precio,descuento){
   const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
   return precioConDescuento;
 };
+
+function onClickPriceDiscount() {
+  const inputPrice = document.getElementById('InputPrice');
+  const priceValue = inputPrice.value;
+
+  const inputDiscount = document.getElementById('InputDiscount');
+  const discountValue = inputDiscount.value;
+
+  const calcPrecioConDescuento = calcularDescuento(priceValue, discountValue);
+
+  const resultPrice = document.getElementById('resultPrice');
+  resultPrice.innerText = 'El precio con descuento es $' + calcPrecioConDescuento;
+}
